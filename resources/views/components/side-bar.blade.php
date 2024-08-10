4e2084a19479382dffd0ddb00592205f63c03f4e
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-08-10 17:51:40 -0400

    Creado AdminController, Componente de AdminLayout, componente de side-bar, vista de dashboard y ruta de dashboard

---
+++ resources/views/components/side-bar.blade.php
@@ -0,0 +1,3 @@
+<div class="w-72 bg-sky-300 dark:bg-sky-900 h-screen overflow-hidden">
+    <x-application-logo divClass="mt-2 p-2" h1Class="md:text-2xl lg:text-2xl" />
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccessDeniedController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -9,15 +10,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified', "is.admin"])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified', "is.admin"])->name('dashboard');
 
 // Routes Access Denied Controller
 Route::get('/access-denied', AccessDeniedController::class)->name('access.denied');
 
 // Routes HomeController
 Route::get("/home", [HomeController::class, "index"])->name("home");
+
+// Routes Admin Controller
+Route::get("/dashboard", [AdminController::class, "index"])->middleware(['auth', 'verified', "is.admin"])->name("dashboard");
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
