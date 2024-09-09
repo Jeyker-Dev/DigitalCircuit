b583dce4e75c23b70583498d5c432340821a8aec
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-09-09 10:18:57 -0400

    Agregado Componente de Livewire para crear categoria y Formulario para Crear Categorias

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -129,7 +129,7 @@
             <!-- Button DropDown Categories -->
             <x-ts-dropdown.items separator>
                 <x-button-side-bar buttonClass="flex justify-start items-center" href="{{route('categories.create')}}">
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-script mx-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 20h-11a3 3 0 0 1 0 -6h11a3 3 0 0 0 0 6h1a3 3 0 0 0 3 -3v-11a2 2 0 0 0 -2 -2h-10a2 2 0 0 0 -2 2v8" /></svg>
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-category mx-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4h6v6h-6z" /><path d="M14 4h6v6h-6z" /><path d="M4 14h6v6h-6z" /><path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>
                     Categories
                 </x-button-side-bar>
             </x-ts-dropdown.items>
