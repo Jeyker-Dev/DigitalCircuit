0191679ec0632e3292a6e2c9fdea1da9fb9e81b2
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-09-11 10:11:51 -0400

    Agregada funcionalidad para eliminar Categorias y Recarga Automatica al Elimnar Una Categoria

--- app/Livewire/ManageCategories.php
+++ app/Livewire/ManageCategories.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Models\Categories;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ManageCategories extends Component
@@ -13,8 +15,15 @@ class ManageCategories extends Component
         $this->categories = $categories;
     }   // Here End Function
 
+    #[On("deleteCategory")]
+    public function deleteCategory(Categories $category)
+    {
+        $category->delete();
+        $this->categories = Categories::all();
+    }   // Here End Function
+
     public function render()
     {
         return view('livewire.manage-categories');
-    }
-}
+    }   // Here End Function
+}   // Here End Class
--- resources/views/livewire/manage-categories.blade.php
+++ resources/views/livewire/manage-categories.blade.php
@@ -7,11 +7,39 @@
             $category->name,
             '<div class="flex items-center justify-center">
             <a class="btn btn-ghost btn-xs bg-green-500 hover:bg-green-510 rounded mr-4 w-16 text-white" href="' . route("categories.edit", $category->id) . '">edit</a>
-            <button class="btn btn-ghost btn-xs bg-red-500 hover:bg-red-510 rounded mr-4 w-16 text-white">delete</button>
+            <button class="btn btn-ghost btn-xs bg-red-500 hover:bg-red-510 rounded mr-4 w-16 text-white" wire:click="$dispatch(\'confirmDelete\', {categoryId:' . $category->id . '})">delete</button>
             </div>'
         ];
     })->toArray();
 @endphp
 
 <x-table :headers="$headers" :data="$data" />
+
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+    document.addEventListener('livewire:init', function () {
+        Livewire.on('confirmDelete', categoryId => {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    @this.call('deleteCategory', {category: categoryId});
+                    Swal.fire(
+                        'Deleted!',
+                        'The Category has been deleted.',
+                        'success'
+                    )
+                }
+            })
+        });
+    });
+</script>
+@endpush
 </div>
